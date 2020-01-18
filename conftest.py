e362ebdead9a445b8102ce9300f5dc51a051bb47
Xavier Francisco <xavier.n.francisco@gmail.com>
2020-01-18 00:34:46 +0000

    Small updates

--- conftest.py
+++ conftest.py
@@ -1,19 +1,11 @@
 "Introduces pytest fixtures that are necessary to run the test suite."
 
-from typing import List
-
 import pytest  # type: ignore
 
-from tests.common import FinalMessage, final_messages, load_power_supply_directory_env
+from tests.common import load_power_supply_directory_env
 
 
 @pytest.fixture(scope="session", autouse=True)
 def load_env() -> None:
     "Fixtures to load the files inside `tests/data` for all tests"
     load_power_supply_directory_env()
-
-
-@pytest.fixture(scope="module", autouse=True)
-def power_supply_1_node_messages() -> List[FinalMessage]:
-    "Returns the power supply for the uvent file in 1_*"
-    return final_messages(1)
--- tests/common.py
+++ tests/common.py
@@ -56,9 +56,9 @@ def power_supplies_for_test() -> Iterator[int]:
 
 def load_power_supply_directory_env() -> None:
     """
-    Load POWER_SUPPLY_DIRECTORY into the tests environment variables.
+    Load POWER_SUPPLY_DIRECTORY into the process environment variables.
 
-    This is used to load the files inside `tests/data`
+    This is used to load the test files inside `tests/data`
     """
     dir_path = Path(os.path.dirname(os.path.realpath(__file__)))
     os.environ["POWER_SUPPLY_DIRECTORY"] = str(dir_path / "data")
